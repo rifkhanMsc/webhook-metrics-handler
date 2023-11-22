312413b138b4296846707575c2bf47bbf1185450
Mohamed Rifkhan <rifkhan.20231646@iit.ac.lk>
2023-11-22 23:55:55 +0530

    just commit

--- src/utils/awsConfig.js
+++ src/utils/awsConfig.js
@@ -4,7 +4,7 @@ const configureAWS = () => {
   config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION || 'us-east-1', // Default to us-east-1 if not provided
+    region: process.env.AWS_REGION || 'us-east-1',  // Default to us-east-1 if not provided
   });
 };
 
