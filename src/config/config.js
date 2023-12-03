c057e41d8ecb79d836b2ea4a360b07c2e8c911c3
rifkhanMsc <150675847+rifkhanMsc@users.noreply.github.com>
2023-12-03 23:31:38 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const aws = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION || 'us-east-1',
+    region: process.env.AWS_REGION || 'us-east-2',
 };
 export const database = {
     connectionString: process.env.DB_CONNECTION_STRING,
